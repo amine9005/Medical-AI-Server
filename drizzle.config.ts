98336539a77647fb9290295b07c1eafd9cd3c33f
amine9005 <amamselmitrader@gmail.com>
2025-11-29 14:16:22 +0100

    Drizzle and neon

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+
+dotenv.config();
+export default defineConfig({
+  out: "./drizzle",
+  schema: "./src/Models",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
---
+++ src/Models/User.Model.ts
@@ -0,0 +1,10 @@
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  credit: integer().notNull().default(0),
+});
+
+export type User = typeof usersTable.$inferSelect;
